65cd829509f048d4daf27ba6d10317270aa06fce
RechkinDenis <52599505+RechkinDenis@users.noreply.github.com>
2024-10-24 15:35:20 +0300

    update

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-    console.log("DOM полностью загружен");
-
     document.querySelectorAll('.opcode-button').forEach(button => {
         button.addEventListener('click', function () {
-            const opcode = this.getAttribute('data-opcode'); // Получаем значение опкода с data-атрибута
-            console.log(`Нажата кнопка с опкодом: ${opcode}`);
-            
-            const targetElement = document.getElementById(opcode); // Используем ID для поиска
-            console.log(`Ищем элемент с id: ${opcode}`, targetElement);
-
+            const opcode = this.getAttribute('data-opcode');
+            const targetElement = document.getElementById(opcode); 
             if (targetElement) {
-                // Скроллим к элементу плавно
-                targetElement.scrollIntoView({ behavior: 'smooth', block: 'center' }); // Скроллим к элементу, но с отступом
-                console.log(`Элемент найден и скроллим к нему: ${opcode}`);
-
-                // Подсветим элемент на время
+                targetElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
                 targetElement.style.backgroundColor = 'yellow';
                 setTimeout(() => {
-                    targetElement.style.backgroundColor = ''; // Убираем подсветку через 1 секунду
+                    targetElement.style.backgroundColor = '';
                 }, 1000);
-            } else {
-                console.error(`Элемент с id ${opcode} не найден`);
             }
         });
     });
